c55446ac7d89e3a8afad19d1c5b0ebaca1027ba5
Nick Yurchenko <zelinskiy1917@gmail.com>
2017-02-13 18:03:04 +0200

    Nice GUI

--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 import cv2
 import numpy as np
 
-def nothing(x):
-    print("1")
-    pass
-cv2.namedWindow('image')
-cv2.createTrackbar('B','image',1,2000,nothing)
+def handleSlidebar(x):
+    drawHough()
 
-img = cv2.imread('test2.png',0)
-prevPos = 1
+def drawHough():
+    print("redraw")
+    p1 = cv2.getTrackbarPos('p1','image')
+    p2 = cv2.getTrackbarPos('p2','image')
+    param1 = cv2.getTrackbarPos('param1','image')
+    param2 = cv2.getTrackbarPos('param2','image')
+    minRadius = cv2.getTrackbarPos('minRadius','image')
+    maxRadius = cv2.getTrackbarPos('maxRadius','image')
 
-while(True):
-    bimg = cv2.medianBlur(img,5)
+    bimg = cv2.GaussianBlur(img,(5,5),0)
+    #bimg = cv2.medianBlur(img,5)
     cimg = cv2.cvtColor(bimg,cv2.COLOR_GRAY2BGR)
 
-    param2 = cv2.getTrackbarPos('B','image')
-    if(param2 != prevPos):
-        circles = cv2.HoughCircles(bimg,cv2.HOUGH_GRADIENT,1,param2,
-                                param1=50,param2=30,minRadius=0,maxRadius=0)
+    circles = cv2.HoughCircles(bimg,cv2.HOUGH_GRADIENT,param1,param2,
+                                param1=50,param2=30,minRadius=0,maxRadius=maxRadius)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-
-        cv2.imshow('image',cimg)
+    cv2.imshow('image',cimg)
 
 
+img = cv2.imread('test4.png',0)
+cv2.namedWindow('image')
+cv2.createTrackbar('param1','image',1,100,handleSlidebar)
+cv2.setTrackbarPos('param1','image',1)
+
+cv2.createTrackbar('param2','image',1,2000,handleSlidebar)
+cv2.setTrackbarPos('param2','image',300)
+
+cv2.createTrackbar('minRadius','image',0,1000,handleSlidebar)
+cv2.setTrackbarPos('minRadius','image',50)
+
+cv2.createTrackbar('maxRadius','image',0,1000,handleSlidebar)
+cv2.setTrackbarPos('maxRadius','image',500)
+
+drawHough()
+
+while(True):
     if cv2.waitKey(1) == 27:
         break
 cv2.destroyAllWindows()
